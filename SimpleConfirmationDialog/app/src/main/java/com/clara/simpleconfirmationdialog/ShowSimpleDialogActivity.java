b897b42f4c493ebc1b7006ae4e1733c4a65a50cb
clara <clara@clara.com>
2016-09-12 10:36:09 -0500

    simple confirmation dialog

---
+++ SimpleConfirmationDialog/app/src/main/java/com/clara/simpleconfirmationdialog/ShowSimpleDialogActivity.java
@@ -0,0 +1,45 @@
+package com.clara.simpleconfirmationdialog;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ShowSimpleDialogActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_simple_dialog);
+
+		Button showDialog = (Button) findViewById(R.id.show_dialog_box);
+		showDialog.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				//Dialogs are created by a Builder.
+				//There's so many possible options when creating a Dialog, so constructors are really awkward.
+				// 'Builder' is the name of a design pattern that tries to address this - Google for more info.
+				//Dialogs belong to an Activity, so provide a reference to this Activity.
+				AlertDialog.Builder builder =
+						new AlertDialog.Builder(ShowSimpleDialogActivity.this);
+
+				//Tell builder which options we'd like. Chain methods together
+				builder.setMessage("I'm a dialog")
+						.setTitle("This is the title")
+						.setPositiveButton(android.R.string.ok, null)
+						.create()      //Create dialog
+						.show();       //Show dialog
+			}
+		});
+	}
+}
+
+
+
+//				//Request the builder creates a Dialog with the specified options
+//				AlertDialog dialog = builder.create();
+//
+//				//And show the dialog
+//				dialog.show();
