d8e3928e79e297d9a2c25d626f7ad11c9c552be6
clara <clara@clara.com>
2016-09-12 10:49:11 -0500

    ok cancel dialog

--- SimpleConfirmationDialogOKCancelButton/app/src/main/java/com/clara/simpleconfirmationdialog/ShowSimpleDialogActivity.java
+++ SimpleConfirmationDialogOKCancelButton/app/src/main/java/com/clara/simpleconfirmationdialog/ShowSimpleDialogActivity.java
@@ -1,17 +1,23 @@
 package com.clara.simpleconfirmationdialog;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ShowSimpleDialogActivity extends AppCompatActivity {
+
+	private TextView mDialogResult;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show_simple_dialog);
+
+		mDialogResult = (TextView) findViewById(R.id.dialog_result_text);
 
 		Button showDialog = (Button) findViewById(R.id.show_dialog_box);
 		showDialog.setOnClickListener(new View.OnClickListener() {
@@ -19,27 +25,35 @@ public class ShowSimpleDialogActivity extends AppCompatActivity {
 			public void onClick(View v) {
 
 				//Dialogs are created by a Builder.
-				//There's so many possible options when creating a Dialog, so constructors are really awkward.
-				// 'Builder' is the name of a design pattern that tries to address this - Google for more info.
-				//Dialogs belong to an Activity, so provide a reference to this Activity.
 				AlertDialog.Builder builder =
 						new AlertDialog.Builder(ShowSimpleDialogActivity.this);
 
 				//Tell builder which options we'd like. Chain methods together
 				builder.setMessage("I'm a dialog")
 						.setTitle("This is the title")
-						.setPositiveButton(android.R.string.ok, null);
-
-				//Request the builder creates a Dialog with the specified options
-				AlertDialog dialog = builder.create();
-
-				//And show the dialog
-				dialog.show();
-
+						.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								userClickedOk();
+							}
+						})
+						.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								userClickedCancel();
+							}
+						})
+						.create()      //Create dialog
+						.show();       //Show dialog
 			}
 		});
 	}
+
+
+	private void userClickedOk() {
+	}
+
+	private void userClickedCancel() {
+	}
+
 }
-
-
-
