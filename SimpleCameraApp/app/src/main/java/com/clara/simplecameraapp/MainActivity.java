29fe8a2af81ec254a3ac746ec8072c6dcfff086c
clara <clara@clara.com>
2016-10-24 22:04:42 -0500

    camera app updated for Nougat

--- SimpleCameraApp/app/src/main/java/com/clara/simplecameraapp/MainActivity.java
+++ SimpleCameraApp/app/src/main/java/com/clara/simplecameraapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
@@ -31,13 +32,12 @@ public class MainActivity extends AppCompatActivity {
 	ImageView mCameraPicture;
 
 	private static int TAKE_PICTURE = 0;
-	private String mImageFilename;
-	private Uri mImageFileUri;
 
-	private static final String PICTURE_TO_DISPLAY = "picture to display key";
-	private static final String IMAGE_FILENAME = "image filename key";
+	private String mImagePath;
+	private Bitmap mImage;
 
-	private boolean mIsPictureToDisplay = false;
+	private static final String IMAGE_FILEPATH_KEY = "image filepath key";
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +45,7 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		if (savedInstanceState != null) {
-			mIsPictureToDisplay = savedInstanceState.getBoolean(PICTURE_TO_DISPLAY, false);
-			mImageFilename = savedInstanceState.getString(IMAGE_FILENAME);
+			mImagePath = savedInstanceState.getString(IMAGE_FILEPATH_KEY);
 		}
 
 		mCameraPicture = (ImageView) findViewById(R.id.camera_picture);
@@ -63,43 +62,40 @@ public class MainActivity extends AppCompatActivity {
 
 		Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-		// Check to see if there is a camera on this device
-
+		// Check to see if there is a camera on this device.
 		if (pictureIntent.resolveActivity(getPackageManager()) == null) {
-			Toast.makeText(MainActivity.this, "Your device does not have a camera",
-					Toast.LENGTH_LONG).show();
+			Toast.makeText(MainActivity.this, "Your device does not have a camera", Toast.LENGTH_LONG).show();
 		}
 
 		else {
 			// Create a File object from the specified filename
-			String filename = "simple_camera_app_" + new Date().getTime();  //Create a unique filename including a timestamp
+			String imageFilename = "simple_camera_app_" + new Date().getTime();  //Create a unique filename including a timestamp
+
 			File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 			File imageFile = null;
+			Uri imageFileUri = null;
+
 			try {
-				imageFile = File.createTempFile(filename, ".jpg", storageDirectory);
+
+				imageFile = File.createTempFile(imageFilename, ".jpg", storageDirectory);
 				Log.i(TAG, "image file " + imageFile);
-				// file: path to use with ACTION_VIEW intents
-				mImageFilename = imageFile.getAbsolutePath();
-				Log.i(TAG, "image file path  " + mImageFilename);
+
+				mImagePath = imageFile.getAbsolutePath();
+				Log.i(TAG, "image file path  " + mImagePath);
+
+				imageFileUri = FileProvider.getUriForFile(MainActivity.this, "com.clara.simplecameraapp", imageFile);
 
 			} catch (IOException ioe) {
 				Log.e(TAG, "Error creating file for photo storage", ioe);
+				return;
 			}
 
-			// And a URI that Android will use to find this file resource
-			// Remember a URI specifies how to find a particular resource (in this case, on the file system),
-			// and where to find it (the file path)
+			//So if the file creation worked, should have a value for imageFileUri. Include this URI as an extra
+			pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
 
-			if (imageFile != null) {
-				mImageFileUri = FileProvider.getUriForFile(MainActivity.this,
-						"com.clara.simplecameraapp", imageFile);
+			//And then request the camera is launched
+			startActivityForResult(pictureIntent, TAKE_PICTURE);
 
-				//Include this URI as an extra
-				pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageFileUri);
-
-				//And then request the camera is launched
-				startActivityForResult(pictureIntent, TAKE_PICTURE);
-			}
 		}
 	}
 
@@ -107,65 +103,67 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+		Log.d(TAG, "On Activity Result");
+
 		if (resultCode == RESULT_OK && requestCode == TAKE_PICTURE) {
-			mIsPictureToDisplay = true;
-		} else {
-			mIsPictureToDisplay = false;
+			scaleBitmap();
+			saveToMediaStore();
+
 		}
 	}
 
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
 		super.onWindowFocusChanged(hasFocus);
-		if (hasFocus && mIsPictureToDisplay) {
-			Bitmap image = scaleBitmap();
-			mCameraPicture.setImageBitmap(image);
-			saveToMediaStore(image);
+		Log.d(TAG, "onWindowFocusChanged");
+
+		if (hasFocus && mImagePath != null) {
+			scaleBitmap();
+			mCameraPicture.setImageBitmap(mImage);
 		}
 	}
 
-	Bitmap mImage;
 
-	private void saveToMediaStore(Bitmap image) {
+
+	private void saveToMediaStore() {
 		//Add image to device's MediaStore - this makes the image accessible to the
 		//gallery app, and other apps that can read from the MediaStore
 
-		//Need to request permission
-
-		mImage = image;
+		//Need to request permission on Nougat and above
 
 		if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-			MediaStore.Images.Media.insertImage(getContentResolver(), image,
-					"SimpleCameraApp", "Photo taken by SimpleCameraApp");
+			MediaStore.Images.Media.insertImage(getContentResolver(), mImage, "SimpleCameraApp", "Photo taken by SimpleCameraApp");
 		} else {
-
+			//This request opens a dialog box for the user to accept the permission request.
+			// When the user clicks ok or cancel, the onRequestPermission method (below) is called with the results
 			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_SAVE_IMAGE_PERMISSION_REQUEST_CODE);
 		}
 
 	}
 
+
+	// This ts the callback for requestPermissions for adding image to media store
+
 	@Override
 	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
 		if (requestCode == REQUEST_SAVE_IMAGE_PERMISSION_REQUEST_CODE) {
-
 			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-				//Now save
-				MediaStore.Images.Media.insertImage(getContentResolver(), mImage,
-						"SimpleCameraApp", "Photo taken by SimpleCameraApp");
-
+				//Now should be able to save
+				MediaStore.Images.Media.insertImage(getContentResolver(), mImage, "SimpleCameraApp", "Photo taken by SimpleCameraApp");
 			}
-
 		}
 	}
 
+	//Handle rotation. Save the image pathname.
+	// Bitmap objects are parcelable so can be put in a bundle, but the bitmap might be huge and take up more
+	// memory that the app is allocated. So, put the file path and re-decode the bitmap on rotation.
 	@Override
 	public void onSaveInstanceState(Bundle outBundle) {
-		outBundle.putBoolean(PICTURE_TO_DISPLAY, mIsPictureToDisplay);
-		outBundle.putString(IMAGE_FILENAME, mImageFilename);
+		outBundle.putString(IMAGE_FILEPATH_KEY, mImagePath);
 	}
 
 
-	private Bitmap scaleBitmap() {
+	private void scaleBitmap() {
 		// * Scale picture taken to fit into the ImageView */      
 		//Step 1: what size is the ImageView?
 		int imageViewHeight = mCameraPicture.getHeight();
@@ -183,11 +181,8 @@ public class MainActivity extends AppCompatActivity {
 		BitmapFactory.Options bOptions = new BitmapFactory.Options();
 		bOptions.inJustDecodeBounds = true;
 
-		//File file = new File(Environment.getExternalStorageDirectory(), mImageFilename);
+		BitmapFactory.decodeFile(mImagePath, bOptions);
 
-		//Uri imageFileUri = Uri.fromFile(file);
-		//String photoFilePath = imageFileUri.getPath();
-		BitmapFactory.decodeFile(mImageFilename, bOptions);      //Get information about the image
 		int pictureHeight = bOptions.outHeight;
 		int pictureWidth = bOptions.outWidth;
 
@@ -195,10 +190,11 @@ public class MainActivity extends AppCompatActivity {
 		int scaleFactor = Math.min(pictureHeight / imageViewHeight, pictureWidth / imageViewWidth);
 
 		//Step 4. Decode the image file into a new bitmap, scaled to fit the ImageView
-		bOptions.inJustDecodeBounds = false;   //now we want to get a bitmap
+		bOptions.inJustDecodeBounds = false;   //setting this to be false will actualy decode the file to a Bitmap
 		bOptions.inSampleSize = scaleFactor;
-		Bitmap bitmap = BitmapFactory.decodeFile(mImageFilename, bOptions);
-		return bitmap;
+
+		Bitmap bitmap = BitmapFactory.decodeFile(mImagePath, bOptions);
+		mImage = bitmap;
 	}
 }
 
