738a4f0b7cf3c452f64e514f8e77cf1cd0a86178
clara <clara@clara.com>
2017-08-28 12:18:32 -0500

    move logic to new Math class

--- HelloTesting/app/src/test/java/com/clara/hellotesting/CalculatorTest.java
+++ HelloTesting/app/src/test/java/com/clara/hellotesting/CalculatorTest.java
@@ -13,12 +13,10 @@ public class CalculatorTest {
 	@Test
 	public void addition_isCorrect() throws Exception {
 
-		CalculatorActivity ca = new CalculatorActivity();
-
-		assertEquals(7, ca.add(3, 4));
-		assertEquals(142, ca.add(42, 100));
-		assertEquals(-43, ca.add(-3, -40));
-		assertEquals(39, ca.add(-3, 42));
+		assertEquals(7, Math.add(3, 4));
+		assertEquals(142, Math.add(42, 100));
+		assertEquals(-43, Math.add(-3, -40));
+		assertEquals(39, Math.add(-3, 42));
 
 	}
 
@@ -26,19 +24,17 @@ public class CalculatorTest {
 	@Test
 	public void verifyInteger_isCorrect() throws Exception {
 
-		CalculatorActivity ca = new CalculatorActivity();
-
 		// Valid integers, as String
-		assertEquals( 45, ca.verifyInputIsNumber("45").intValue());
-		assertEquals( 123456, ca.verifyInputIsNumber("123456").intValue());
-		assertEquals( -23423445, ca.verifyInputIsNumber("-23423445").intValue());
+		assertEquals( 45, Math.verifyInputIsNumber("45").intValue());
+		assertEquals( 123456, Math.verifyInputIsNumber("123456").intValue());
+		assertEquals( -23423445, Math.verifyInputIsNumber("-23423445").intValue());
 
 		// Invalid integers should return null
-		assertNull(ca.verifyInputIsNumber("45.6"));      // decimal place
-		assertNull(ca.verifyInputIsNumber("4sdfsdf56"));   // characters
-		assertNull(ca.verifyInputIsNumber("4.5.6"));      // malformed number
-		assertNull(ca.verifyInputIsNumber("pizza"));      // letters
-		assertNull(ca.verifyInputIsNumber("2345675432567543567"));  // too long for an int
+		assertNull(Math.verifyInputIsNumber("45.6"));      // decimal place
+		assertNull(Math.verifyInputIsNumber("4sdfsdf56"));   // characters
+		assertNull(Math.verifyInputIsNumber("4.5.6"));      // malformed number
+		assertNull(Math.verifyInputIsNumber("pizza"));      // letters
+		assertNull(Math.verifyInputIsNumber("2345675432567543567"));  // too long for an int
 	}
 
 
