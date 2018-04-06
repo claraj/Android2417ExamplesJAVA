a4f5b52948f6d74cdb1cabc973cc6fb0f4d17f77
clara <clara@minneapolis.edu>
2018-04-06 11:13:18 -0500

    example random compliment API app using Volley

---
+++ Hello_Compliment_API/app/src/main/java/com/clara/hello_compliment_api/MainActivity.java
@@ -0,0 +1,74 @@
+package com.clara.hello_compliment_api;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class MainActivity extends AppCompatActivity {
+
+	private Button mGetComplimentButton;
+	private TextView mShowComplimentText;
+
+	private static final String url = "";  // Insert your URL here e.g. "https://happy-kitten-234.herokuapp.com/random"
+
+	private static final String TAG = "Random Compliment Activity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mGetComplimentButton = findViewById(R.id.get_compliment_btn);
+		mShowComplimentText = findViewById(R.id.compliment_text);
+
+		mGetComplimentButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				getRandomCompliment();
+			}
+		});
+	}
+
+	public void getRandomCompliment() {
+
+		RequestQueue queue = Volley.newRequestQueue(this);
+
+		JsonObjectRequest complimentRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+				new Response.Listener<JSONObject>() {
+					@Override
+					public void onResponse(JSONObject response) {
+
+						try {
+							String compliment = response.getString("text");
+							mShowComplimentText.setText(compliment);
+						} catch (JSONException e) {
+							Log.e(TAG, "Error processing JSON response", e);
+						}
+					}
+				},
+				new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						Log.e(TAG, "Error fetching data from compliment server", error);
+					}
+				}
+		);
+
+		queue.add(complimentRequest);
+	}
+
+}
