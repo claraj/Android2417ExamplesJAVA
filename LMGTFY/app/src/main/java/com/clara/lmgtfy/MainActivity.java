1184eda80523da68a7a6bab0129ae16f3d46fb0a
clara <clara@minneapolis.edu>
2019-08-18 16:21:10 -0500

    LMGTFY 2019

--- LMGTFY/app/src/main/java/com/clara/lmgtfy/MainActivity.java
+++ LMGTFY/app/src/main/java/com/clara/lmgtfy/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.KeyListener;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,41 +16,57 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    private EditText searchText;
+    private Button searchButton;
+    private TextView searchConfirmation;
 
-		//Get references to widgets - the Button, EditText and show_search_text TextViww
-		Button webSearchButton = (Button) findViewById(R.id.google_search_button);
-		// Any widgets you refer to from within an event handler must be declared final
-		final EditText webSearchBox = (EditText) findViewById(R.id.google_search_box);
-		final TextView confirmSearchText = (TextView) findViewById(R.id.show_search_text);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		webSearchButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
+        searchText = findViewById(R.id.enter_search_text);
+        searchButton = findViewById(R.id.search_button);
+        searchConfirmation = findViewById(R.id.show_search_text);
 
-				//Read the text in the EditText box
-				String searchString = webSearchBox.getText().toString();
-				//Display that to the user to notify that the search is about to begin...
-				confirmSearchText.setText("Searching for " + searchString);
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
 
-				//Display confirmation Toast
-				Toast.makeText(MainActivity.this, "Searching for " + searchString, Toast.LENGTH_LONG).show();
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
 
-				//Create Intent to launch web browser
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Read text, and use it to set the text in searchConfirmation
+                String text = s.length() == 0 ? "..." : s.toString();
+                searchConfirmation.setText(getString(R.string.search_display_text, text));
 
-				//First, create search URI
-				String uriText = "https://www.google.com/search?q=" + searchString;
-				Uri searchUri = Uri.parse(uriText);
+            }
+        });
 
-				//Create Intent with this URI and the web browse action
-				Intent browserIntent = new Intent(Intent.ACTION_VIEW, searchUri);
-				//Send Intent to Android syatem to start web browser Activity
-				startActivity(browserIntent);
-			}
-		});
-	}
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Read text in EditText
+                String search = searchText.getText().toString();
+                if (search.isEmpty()) { return; }
+
+                // show Toast confirmation
+                Toast confirmation = Toast.makeText(MainActivity.this, "Searching for " + search, Toast.LENGTH_LONG);
+                confirmation.show();
+
+                // Launch web browser with URI for Google search
+                String uriString = "https://www.google.com/search?q=" + search;
+                Uri uri = Uri.parse(uriString);
+
+                Intent launchBrowserIntent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(launchBrowserIntent);
+
+            }
+        });
+    }
 }
+
+
 
