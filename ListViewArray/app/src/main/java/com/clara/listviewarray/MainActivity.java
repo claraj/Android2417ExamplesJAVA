09efbfe4a92f64f7c96680a8f8e79857f74a533f
clara <clara@minneapolis.edu>
2019-09-15 14:30:06 -0500

    update basic listview for 29

--- ListViewArray/app/src/main/java/com/clara/listviewarray/MainActivity.java
+++ ListViewArray/app/src/main/java/com/clara/listviewarray/MainActivity.java
@@ -2,16 +2,20 @@ package com.clara.listviewarray;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ListViewCompat;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+	//An array to provide data for the list
+	private final String[] mAndroidVersions = {"Jellybean", "Kitkat", "Lollipop",
+			"Marshmallow", "Oreo", "Nougat", "Pie"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +25,24 @@ public class MainActivity extends AppCompatActivity {
 		//Get a reference to the ListView
 		 ListView listView = (ListView) findViewById(R.id.simple_list);
 
-		//An array to provide some data for the list
-		String[] androidVersions = {"Ice cream sandwich", "Jellybean", "Kitkat", "Lollipop"};
-
-		//Create an ArrayAdapter. Note generic type <String> used
-		//Arguments are Context, a TextView's resource ID, a List of the generic type
+		//Create an ArrayAdapter, note generic type <String> used
+		//Arguments are Context, the TextView's resource ID, a List or array of the generic type
 		ArrayAdapter<String> arrayAdapter =
-				new ArrayAdapter<String>(this, R.layout.list_item, R.id.list_item_text, androidVersions);
+				new ArrayAdapter<>(this, R.layout.list_item, R.id.list_item_text, mAndroidVersions);
 
 		//And set this ArrayAdapter to be the the ListView's adapter
 		listView.setAdapter(arrayAdapter);
 
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				// The positions in the list correspond to array indexes
+				String itemText = mAndroidVersions[position];
+				Toast.makeText(MainActivity.this, itemText, Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
+
 
 
