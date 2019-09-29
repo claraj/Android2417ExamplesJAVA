84bcdf8bbbf5e8fb70d9ae8f0570b752377e96f3
clara <clara@minneapolis.edu>
2019-09-29 12:00:24 -0500

    updates for 2019 class

--- SimpleCameraThumbnailPhoto/app/src/main/java/com/clara/simplecamerathumbnailphoto/MainActivity.java
+++ SimpleCameraThumbnailPhoto/app/src/main/java/com/clara/simplecamerathumbnailphoto/MainActivity.java
@@ -3,17 +3,18 @@ package com.clara.simplecamerathumbnailphoto;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-	Button mTakePictureButton;
-	ImageView mCameraPicture;
+	private Button mTakePictureButton;
+	private ImageView mCameraPicture;
 
 	private static int TAKE_PICTURE_REQUEST_CODE = 0;
 
@@ -22,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		mCameraPicture = (ImageView) findViewById(R.id.camera_picture);
-		mTakePictureButton = (Button) findViewById(R.id.take_picture_button);
+		mCameraPicture = findViewById(R.id.camera_picture);
+		mTakePictureButton = findViewById(R.id.take_picture_button);
 
 		mTakePictureButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -31,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 				takeThumbnailPicture();
 			}
 		});
-
 	}
 
 	private void takeThumbnailPicture() {
@@ -45,13 +45,11 @@ public class MainActivity extends AppCompatActivity {
 		} else {
 			Toast.makeText(this, "Your device does not have a camera app", Toast.LENGTH_LONG).show();
 		}
-
 	}
 
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
 		if (requestCode == TAKE_PICTURE_REQUEST_CODE && resultCode == RESULT_OK) {
 			Bitmap thumbnail = data.getParcelableExtra("data");
 			mCameraPicture.setImageBitmap(thumbnail);
