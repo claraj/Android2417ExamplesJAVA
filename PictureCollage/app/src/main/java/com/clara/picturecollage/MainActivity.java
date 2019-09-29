2c2c40668e6e13abaae16e92b369dc7646828d05
clara <clara@minneapolis.edu>
2019-09-29 09:36:40 -0500

    collage layout

---
+++ PictureCollage/app/src/main/java/com/clara/picturecollage/MainActivity.java
@@ -0,0 +1,23 @@
+package com.clara.picturecollage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
